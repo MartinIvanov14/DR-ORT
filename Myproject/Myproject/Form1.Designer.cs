4ef47919c0a25f7d07a648370ff671262d9bc5a1
MartinIvanov14 <118364914+MartinIvanov14@users.noreply.github.com>
2024-06-12 12:42:22 +0300

    Add files via upload

---
+++ Myproject/Myproject/Form1.Designer.cs
@@ -0,0 +1,364 @@
+﻿
+namespace Myproject
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.rebtextBox12 = new System.Windows.Forms.TextBox();
+            this.rebtextBox13 = new System.Windows.Forms.TextBox();
+            this.rebtextBox23 = new System.Windows.Forms.TextBox();
+            this.rebtextBox24 = new System.Windows.Forms.TextBox();
+            this.rebtextBox46 = new System.Windows.Forms.TextBox();
+            this.rebtextBox25 = new System.Windows.Forms.TextBox();
+            this.rebtextBox45 = new System.Windows.Forms.TextBox();
+            this.rebtextBox56 = new System.Windows.Forms.TextBox();
+            this.rebtextBox67 = new System.Windows.Forms.TextBox();
+            this.rebtextBox35 = new System.Windows.Forms.TextBox();
+            this.rebtextBox57 = new System.Windows.Forms.TextBox();
+            this.Startbutton = new System.Windows.Forms.Button();
+            this.resul = new System.Windows.Forms.Label();
+            this.rezlabel = new System.Windows.Forms.Label();
+            this.goaltextBox = new System.Windows.Forms.TextBox();
+            this.tryAgainbutton = new System.Windows.Forms.Button();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(538, 174);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(432, 175);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 9;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label1.Location = new System.Drawing.Point(505, 240);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(40, 40);
+            this.label1.TabIndex = 10;
+            this.label1.Text = "1";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label2
+            // 
+            this.label2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label2.Location = new System.Drawing.Point(662, 142);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(40, 40);
+            this.label2.TabIndex = 11;
+            this.label2.Text = "2";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label3
+            // 
+            this.label3.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label3.Location = new System.Drawing.Point(662, 337);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(40, 40);
+            this.label3.TabIndex = 12;
+            this.label3.Text = "3";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label4
+            // 
+            this.label4.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label4.Location = new System.Drawing.Point(803, 142);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(40, 40);
+            this.label4.TabIndex = 13;
+            this.label4.Text = "4";
+            this.label4.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label5
+            // 
+            this.label5.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label5.Location = new System.Drawing.Point(803, 337);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(40, 40);
+            this.label5.TabIndex = 14;
+            this.label5.Text = "5";
+            this.label5.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label6
+            // 
+            this.label6.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label6.Location = new System.Drawing.Point(959, 154);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(40, 40);
+            this.label6.TabIndex = 15;
+            this.label6.Text = "6";
+            this.label6.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label7
+            // 
+            this.label7.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label7.Location = new System.Drawing.Point(959, 337);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(40, 40);
+            this.label7.TabIndex = 16;
+            this.label7.Text = "7";
+            this.label7.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // rebtextBox12
+            // 
+            this.rebtextBox12.Location = new System.Drawing.Point(578, 211);
+            this.rebtextBox12.Name = "rebtextBox12";
+            this.rebtextBox12.Size = new System.Drawing.Size(54, 22);
+            this.rebtextBox12.TabIndex = 17;
+            this.rebtextBox12.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // rebtextBox13
+            // 
+            this.rebtextBox13.Location = new System.Drawing.Point(578, 290);
+            this.rebtextBox13.Name = "rebtextBox13";
+            this.rebtextBox13.Size = new System.Drawing.Size(54, 22);
+            this.rebtextBox13.TabIndex = 18;
+            this.rebtextBox13.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // rebtextBox23
+            // 
+            this.rebtextBox23.Location = new System.Drawing.Point(648, 249);
+            this.rebtextBox23.Name = "rebtextBox23";
+            this.rebtextBox23.Size = new System.Drawing.Size(54, 22);
+            this.rebtextBox23.TabIndex = 19;
+            this.rebtextBox23.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // rebtextBox24
+            // 
+            this.rebtextBox24.Location = new System.Drawing.Point(725, 163);
+            this.rebtextBox24.Name = "rebtextBox24";
+            this.rebtextBox24.Size = new System.Drawing.Size(54, 22);
+            this.rebtextBox24.TabIndex = 20;
+            this.rebtextBox24.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // rebtextBox46
+            // 
+            this.rebtextBox46.Location = new System.Drawing.Point(872, 163);
+            this.rebtextBox46.Name = "rebtextBox46";
+            this.rebtextBox46.Size = new System.Drawing.Size(54, 22);
+            this.rebtextBox46.TabIndex = 21;
+            this.rebtextBox46.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // rebtextBox25
+            // 
+            this.rebtextBox25.Location = new System.Drawing.Point(739, 258);
+            this.rebtextBox25.Name = "rebtextBox25";
+            this.rebtextBox25.Size = new System.Drawing.Size(54, 22);
+            this.rebtextBox25.TabIndex = 22;
+            this.rebtextBox25.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // rebtextBox45
+            // 
+            this.rebtextBox45.Location = new System.Drawing.Point(803, 230);
+            this.rebtextBox45.Name = "rebtextBox45";
+            this.rebtextBox45.Size = new System.Drawing.Size(54, 22);
+            this.rebtextBox45.TabIndex = 23;
+            this.rebtextBox45.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // rebtextBox56
+            // 
+            this.rebtextBox56.Location = new System.Drawing.Point(857, 272);
+            this.rebtextBox56.Name = "rebtextBox56";
+            this.rebtextBox56.Size = new System.Drawing.Size(54, 22);
+            this.rebtextBox56.TabIndex = 24;
+            this.rebtextBox56.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // rebtextBox67
+            // 
+            this.rebtextBox67.Location = new System.Drawing.Point(945, 249);
+            this.rebtextBox67.Name = "rebtextBox67";
+            this.rebtextBox67.Size = new System.Drawing.Size(54, 22);
+            this.rebtextBox67.TabIndex = 25;
+            this.rebtextBox67.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // rebtextBox35
+            // 
+            this.rebtextBox35.Location = new System.Drawing.Point(725, 337);
+            this.rebtextBox35.Name = "rebtextBox35";
+            this.rebtextBox35.Size = new System.Drawing.Size(54, 22);
+            this.rebtextBox35.TabIndex = 26;
+            this.rebtextBox35.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // rebtextBox57
+            // 
+            this.rebtextBox57.Location = new System.Drawing.Point(872, 337);
+            this.rebtextBox57.Name = "rebtextBox57";
+            this.rebtextBox57.Size = new System.Drawing.Size(54, 22);
+            this.rebtextBox57.TabIndex = 27;
+            this.rebtextBox57.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // Startbutton
+            // 
+            this.Startbutton.Location = new System.Drawing.Point(17, 201);
+            this.Startbutton.Name = "Startbutton";
+            this.Startbutton.Size = new System.Drawing.Size(85, 43);
+            this.Startbutton.TabIndex = 28;
+            this.Startbutton.Text = "Старт";
+            this.Startbutton.UseVisualStyleBackColor = true;
+            this.Startbutton.Click += new System.EventHandler(this.Startbutton_Click);
+            // 
+            // resul
+            // 
+            this.resul.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.resul.Location = new System.Drawing.Point(12, 295);
+            this.resul.Name = "resul";
+            this.resul.Size = new System.Drawing.Size(96, 23);
+            this.resul.TabIndex = 29;
+            this.resul.Text = "Резултат";
+            this.resul.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // rezlabel
+            // 
+            this.rezlabel.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.rezlabel.Location = new System.Drawing.Point(12, 340);
+            this.rezlabel.Name = "rezlabel";
+            this.rezlabel.Size = new System.Drawing.Size(500, 65);
+            this.rezlabel.TabIndex = 30;
+            // 
+            // goaltextBox
+            // 
+            this.goaltextBox.Location = new System.Drawing.Point(27, 137);
+            this.goaltextBox.Name = "goaltextBox";
+            this.goaltextBox.Size = new System.Drawing.Size(58, 22);
+            this.goaltextBox.TabIndex = 31;
+            this.goaltextBox.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // tryAgainbutton
+            // 
+            this.tryAgainbutton.Location = new System.Drawing.Point(166, 201);
+            this.tryAgainbutton.Name = "tryAgainbutton";
+            this.tryAgainbutton.Size = new System.Drawing.Size(85, 43);
+            this.tryAgainbutton.TabIndex = 32;
+            this.tryAgainbutton.Text = "Нов опит";
+            this.tryAgainbutton.UseVisualStyleBackColor = true;
+            this.tryAgainbutton.Click += new System.EventHandler(this.tryAgainbutton_Click);
+            // 
+            // label8
+            // 
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label8.Location = new System.Drawing.Point(24, 43);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(271, 30);
+            this.label8.TabIndex = 33;
+            this.label8.Text = "Началният възел е възел едно\r\n\r\n";
+            // 
+            // label9
+            // 
+            this.label9.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label9.Location = new System.Drawing.Point(24, 86);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(396, 48);
+            this.label9.TabIndex = 34;
+            this.label9.Text = "Изберете възела, до който искате да стигнете\r\n(изберете цяло число между две и се" +
+    "дем):\r\n\r\n\r\n\r\n";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1052, 456);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.tryAgainbutton);
+            this.Controls.Add(this.goaltextBox);
+            this.Controls.Add(this.rezlabel);
+            this.Controls.Add(this.resul);
+            this.Controls.Add(this.Startbutton);
+            this.Controls.Add(this.rebtextBox57);
+            this.Controls.Add(this.rebtextBox35);
+            this.Controls.Add(this.rebtextBox67);
+            this.Controls.Add(this.rebtextBox56);
+            this.Controls.Add(this.rebtextBox45);
+            this.Controls.Add(this.rebtextBox25);
+            this.Controls.Add(this.rebtextBox46);
+            this.Controls.Add(this.rebtextBox24);
+            this.Controls.Add(this.rebtextBox23);
+            this.Controls.Add(this.rebtextBox13);
+            this.Controls.Add(this.rebtextBox12);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "Form1";
+            this.Text = "Uniform Cost Search";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox rebtextBox12;
+        private System.Windows.Forms.TextBox rebtextBox13;
+        private System.Windows.Forms.TextBox rebtextBox23;
+        private System.Windows.Forms.TextBox rebtextBox24;
+        private System.Windows.Forms.TextBox rebtextBox46;
+        private System.Windows.Forms.TextBox rebtextBox25;
+        private System.Windows.Forms.TextBox rebtextBox45;
+        private System.Windows.Forms.TextBox rebtextBox56;
+        private System.Windows.Forms.TextBox rebtextBox67;
+        private System.Windows.Forms.TextBox rebtextBox35;
+        private System.Windows.Forms.TextBox rebtextBox57;
+        private System.Windows.Forms.Button Startbutton;
+        private System.Windows.Forms.Label resul;
+        private System.Windows.Forms.Label rezlabel;
+        private System.Windows.Forms.TextBox goaltextBox;
+        private System.Windows.Forms.Button tryAgainbutton;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+    }
+}
+
---
+++ Myproject/Myproject/Form1.cs
@@ -0,0 +1,231 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Myproject
+{
+    public partial class Form1 : Form
+    {
+
+        public class Node
+        {
+            public int Id { get; set; }
+            public List<Edge> Edges { get; set; }
+            
+        }
+
+        public class Edge
+        {
+            public Node To { get; set; }
+            public int Cost { get; set; }
+        }
+
+        public class PriorityQueue<T>
+        {
+            private SortedDictionary<int, Queue<T>> list = new SortedDictionary<int, Queue<T>>();
+
+            public void Enqueue(T item, int priority)
+            {
+
+                if (!list.ContainsKey(priority))
+                {
+                    list[priority] = new Queue<T>();
+                }
+                list[priority].Enqueue(item);
+
+            }
+
+            public T Dequeue()
+            {
+
+                var firstPair = list.OrderBy(p => p.Key).First();
+                var dequeuedItem = firstPair.Value.Dequeue();
+                if (firstPair.Value.Count == 0)
+                {
+                    list.Remove(firstPair.Key);
+                }
+                return dequeuedItem;
+
+            }
+
+            public bool IsEmpty
+            {
+                get { return list.Count == 0; }
+            }
+
+        }
+
+        public List<TextBox> textBoxes;
+
+        List<TextBox> rebtextBoxes;
+        public Form1()
+        {
+            InitializeComponent();
+
+            textBoxes = new List<TextBox>() { rebtextBox12, rebtextBox13, rebtextBox23, rebtextBox24, rebtextBox25, rebtextBox35,
+            rebtextBox45, rebtextBox46, rebtextBox56, rebtextBox57, rebtextBox67, goaltextBox};
+            rebtextBoxes = new List<TextBox>() { rebtextBox12, rebtextBox13, rebtextBox23, rebtextBox24, rebtextBox25, rebtextBox35,
+            rebtextBox45, rebtextBox46, rebtextBox56, rebtextBox57, rebtextBox67};
+        }
+
+        public static (List<Node>, Dictionary<Node, int>) UniformCostSearch(Node start, Node goal)
+        {
+            var visited = new HashSet<Node>();
+            var queue = new PriorityQueue<Node>();
+            var cameFrom = new Dictionary<Node, Node>();
+            var costSoFar = new Dictionary<Node, int>();
+
+            queue.Enqueue(start, 0);
+            cameFrom[start] = null;
+            costSoFar[start] = 0;
+
+            while (!queue.IsEmpty)
+            {
+                var current = queue.Dequeue();
+
+                if (current == goal)
+                {
+                    var path = new List<Node>();
+                    while (current != null)
+                    {
+                        path.Add(current);
+                        current = cameFrom[current];
+                    }
+                    path.Reverse();
+                    return (path, costSoFar);
+                }
+
+                visited.Add(current);
+
+                foreach (var edge in current.Edges)
+                {
+                    var next = edge.To;
+                    var newCost = costSoFar[current] + edge.Cost;
+
+                    if (!costSoFar.ContainsKey(next) || newCost < costSoFar[next])
+                    {
+                        if (!visited.Contains(next))
+                        {
+                            costSoFar[next] = newCost;
+                            queue.Enqueue(next, newCost);
+                            cameFrom[next] = current;
+                        }
+                    }
+                }
+            }
+
+            return (null, new Dictionary<Node, int>());
+        }
+
+        private void Startbutton_Click(object sender, EventArgs e)
+        {
+            var nodes = new Node[7];
+            for (int i = 0; i < 7; i++)
+                nodes[i] = new Node { Id = i + 1, Edges = new List<Edge>() };
+
+
+            string costText12 = rebtextBox12.Text;
+            string costText13 = rebtextBox13.Text;
+            string costText23 = rebtextBox23.Text;
+            string costText24 = rebtextBox24.Text;
+            string costText25 = rebtextBox25.Text;
+            string costText35 = rebtextBox35.Text;
+            string costText45 = rebtextBox45.Text;
+            string costText46 = rebtextBox46.Text;
+            string costText56 = rebtextBox56.Text;
+            string costText57 = rebtextBox57.Text;
+            string costText67 = rebtextBox67.Text;
+            foreach (TextBox textBox in textBoxes)
+            {
+                if (string.IsNullOrWhiteSpace(textBox.Text))
+                {
+                    MessageBox.Show("Моля, попълнете празните полета.");
+                    textBox.Focus();
+                    return;
+                }
+            }
+            if (!int.TryParse(costText12, out int cost12) || !int.TryParse(costText13, out int cost13)|| !int.TryParse(costText23, out int cost23)||
+                !int.TryParse(costText24, out int cost24)|| !int.TryParse(costText25, out int cost25)|| !int.TryParse(costText35, out int cost35)||
+                !int.TryParse(costText45, out int cost45)|| !int.TryParse(costText46, out int cost46)|| !int.TryParse(costText56, out int cost56)||
+                !int.TryParse(costText57, out int cost57)|| !int.TryParse(costText67, out int cost67))
+            {
+                MessageBox.Show("Моля, въвеждайте само цели числа в полетата за ребрата.");
+                return;
+            }
+
+            nodes[0].Edges.Add(new Edge { To = nodes[1], Cost = cost12 }); nodes[0].Edges.Add(new Edge { To = nodes[2], Cost = cost13 });
+            nodes[1].Edges.Add(new Edge { To = nodes[0], Cost = cost12 }); nodes[1].Edges.Add(new Edge { To = nodes[2], Cost = cost23 });
+            nodes[1].Edges.Add(new Edge { To = nodes[3], Cost = cost24 }); nodes[1].Edges.Add(new Edge { To = nodes[4], Cost = cost25 });
+            nodes[2].Edges.Add(new Edge { To = nodes[0], Cost = cost13 }); nodes[2].Edges.Add(new Edge { To = nodes[1], Cost = cost23 });
+            nodes[2].Edges.Add(new Edge { To = nodes[4], Cost = cost35 });
+            nodes[3].Edges.Add(new Edge { To = nodes[1], Cost = cost24 }); nodes[3].Edges.Add(new Edge { To = nodes[4], Cost = cost45 });
+            nodes[3].Edges.Add(new Edge { To = nodes[5], Cost = cost46 });
+            nodes[4].Edges.Add(new Edge { To = nodes[1], Cost = cost25 }); nodes[4].Edges.Add(new Edge { To = nodes[2], Cost = cost35 });
+            nodes[4].Edges.Add(new Edge { To = nodes[3], Cost = cost45 }); nodes[4].Edges.Add(new Edge { To = nodes[5], Cost = cost56 });
+            nodes[4].Edges.Add(new Edge { To = nodes[6], Cost = cost57 });
+            nodes[5].Edges.Add(new Edge { To = nodes[3], Cost = cost46 }); nodes[5].Edges.Add(new Edge { To = nodes[4], Cost = cost56 });
+            nodes[5].Edges.Add(new Edge { To = nodes[6], Cost = cost67 });
+            nodes[6].Edges.Add(new Edge { To = nodes[4], Cost = cost57 }); nodes[6].Edges.Add(new Edge { To = nodes[5], Cost = cost67 });
+
+            string goalText = goaltextBox.Text;
+            int goal;
+            if (!int.TryParse(goaltextBox.Text, out goal) || goal < 2 || goal > 7)
+            {
+                MessageBox.Show("Моля, въведете цяло число между две и седем в полето за целевия възел.");
+                goaltextBox.Focus();
+                return;
+            }
+
+            int cost;
+
+            foreach (TextBox textBox in rebtextBoxes)
+            {
+                if (!int.TryParse(textBox.Text, out cost) || cost <= 0)
+                {
+                    MessageBox.Show("Моля, въвеждайте положителни цели числа в полетата за ребрата.");
+                    textBox.Focus();
+                    return;
+                }
+            }
+
+            var (pathToNode, costSoFar) = UniformCostSearch(nodes[0], nodes[goal-1]);
+            if (pathToNode != null)
+            {
+                var pathCost = costSoFar[pathToNode.Last()];
+                string path = "Пътят от възел 1 до възел " +  goalText + " е: " + string.Join(" -> ", pathToNode.Select(n => n.Id))
+                            + " с обща стойност: " + pathCost;
+                rezlabel.Text = path;
+            }
+            else
+            {
+                Console.WriteLine("Няма път от възел 1 до възел"+ goalText);
+            }
+
+            foreach (TextBox textBox in textBoxes)
+            {
+                textBox.Enabled = false;
+            }
+        }
+
+        private void tryAgainbutton_Click(object sender, EventArgs e)
+        {
+            foreach (TextBox textBox in textBoxes)
+            {
+                textBox.Clear();
+            }
+
+            rezlabel.Text = " ";
+
+            foreach (TextBox textBox in textBoxes)
+            {
+                textBox.Enabled = true;
+            }
+        }
+    }
+}
